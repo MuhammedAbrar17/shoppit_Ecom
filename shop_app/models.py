12b17a990c1519be8da20fd8bf6119e1cb7cb895
Abu <abuabrar520724@gmail.com>
2025-03-11 23:03:54 +0530

    first commit

---
+++ shop_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    CATEGORY = (("Electronics", "ELECTRONICS"),
+                ("Groceries", "GROCERIES"),
+                ("Clothings", "CLOTHINGS")
+                )
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True, null=True)
+    image = models.ImageField(upload_to="img")
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100, choices=CATEGORY, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
